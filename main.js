134f4e6dce187dd418f6573b2cff16f4596a635f
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-10-26 14:48:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-alert("Hello World")
-let timeOfDay = "Morning";
-alert("This is Java script"+ timeOfDay)
-prompt("What is your name")
-let username = "Mr. Fields";
-alert(username)
+alert("Hello World");
+let timeOfDay = prompt("What time of day is it")
+let username = prompt ("what is your name ")
+let(msg) = Hello $(username). Have a great $(timeOfDay)
+
+alert(msg)
