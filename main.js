717c8a899e7dd79204266eb4fd5d14b7673920ae
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-10-28 13:49:03 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-alert("Hello World");
-let timeOfDay = prompt("What time of day is it")
-let username = prompt ("what is your name ")
-let(msg) = Hello $(username). Have a great $(timeOfDay)
+//JavaScript code
+alert("hello  world")
+let timeOfDay = prompt("What time of day is it?");
+let username = prompt ("what is your name?");
 
-alert(msg)
+let msg = 'Hello $(username). Have a great $(timeOfDay)!';
+
+alert(msg);
